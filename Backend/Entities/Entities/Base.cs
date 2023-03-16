acce3fdab016e4487e2fedd44f18444430b4dcd7
hmarconef <humberto.freitas@ionsistemas.com.br>
2023-03-16 16:39:34 -0300

    Parte 3 - Sistema Financeiro DDD - Angular - NET 7 - 2023 - Criando Entidades relacionadas

---
+++ Backend/Entities/Entities/Base.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Notification;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Entities;
+
+public class Base : Notifica
+{
+    [Display(Name = "Código")]
+    public int Id { get; set; }
+
+    [Display(Name = "Nome")]
+    public string? Nome { get; set; }
+}
