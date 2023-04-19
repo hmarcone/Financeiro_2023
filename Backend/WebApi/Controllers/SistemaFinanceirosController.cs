8102e0f226e6e099d3338dfae0241e2982142e1d
hmarconef <humberto.freitas@ionsistemas.com.br>
2023-04-19 14:39:25 -0300

    Parte 9 - Financeiro DDD Angular - NET 7 - 2023 - Criação das Apis gerenciamento do Sistema

---
+++ Backend/WebApi/Controllers/SistemaFinanceirosController.cs
@@ -0,0 +1,74 @@
+﻿using Domain.Interfaces.InterfaceServicos;
+using Domain.Interfaces.ISistemaFinanceiro;
+using Domain.Services;
+using Entities.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize]
+public class SistemaFinanceirosController : ControllerBase
+{
+    private readonly InterfaceSistemaFinanceiro _interfaceSistemaFinanceiro;
+    private readonly ISistemaFinanceiroServico _iSistemaFinanceiroServico;
+
+    public SistemaFinanceirosController(InterfaceSistemaFinanceiro InterfaceSistemaFinanceiro,
+        ISistemaFinanceiroServico ISistemaFinanceiroServico)
+    {
+        _interfaceSistemaFinanceiro = InterfaceSistemaFinanceiro;
+        _iSistemaFinanceiroServico = ISistemaFinanceiroServico;
+    }
+
+    [HttpGet("/api/ListaSistemasUsuario")]
+    [Produces("application/json")]
+    public async Task<object> ListaSistemasUsuario(string emailUsuario)
+    {
+        return await _interfaceSistemaFinanceiro.ListaSistemasUsuario(emailUsuario);
+    }
+
+    [HttpPost("/api/AdicionarSistemaFinanceiro")]
+    [Produces("application/json")]
+    public async Task<object> AdicionarSistemaFinanceiro(SistemaFinanceiro sistemaFinanceiro)
+    {
+        await _iSistemaFinanceiroServico.AdicionarSistemaFinanceiro(sistemaFinanceiro);
+
+        return Task.FromResult(sistemaFinanceiro);
+    }
+
+    [HttpPut("/api/AtualizarSistemaFinanceiro")]
+    [Produces("application/json")]
+    public async Task<object> AtualizarSistemaFinanceiro(SistemaFinanceiro sistemaFinanceiro)
+    {
+        await _iSistemaFinanceiroServico.AtualizarSistemaFinanceiro(sistemaFinanceiro);
+
+        return Task.FromResult(sistemaFinanceiro);
+    }
+
+    [HttpGet("/api/ObterSistemaFinanceiro")]
+    [Produces("application/json")]
+    public async Task<object> ObterSistemaFinanceiro(int id)
+    {
+        return await _interfaceSistemaFinanceiro.GetEntityById(id);
+    }
+
+
+    [HttpDelete("/api/DeleteSistemaFinanceiro")]
+    [Produces("application/json")]
+    public async Task<object> DeleteSistemaFinanceiro(int id)
+    {
+        try
+        {
+            var sistemaFinanceiro = await _interfaceSistemaFinanceiro.GetEntityById(id);
+
+            await _interfaceSistemaFinanceiro.Delete(sistemaFinanceiro);
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+        return true;
+    }
+}
