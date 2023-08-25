571ee7dc5b0e0d88da89b22e535f0388eb8b8b30
humberto.freitas <humberto.freitas@ionsistemas.com.br>
2023-08-25 16:23:43 -0300

    AULA 6 - ANGULAR - PROJETO FINANCEIRO (FRONT-END) Layout das paginas e primeira pagina do sistema

--- Frontend/my-financeiro/src/app/pages/sistema/sistema.module.ts
+++ Frontend/my-financeiro/src/app/pages/sistema/sistema.module.ts
@@ -5,10 +5,17 @@ import { NavbarModule } from 'src/app/components/navbar/navbar.module';
 import { SidebarModule } from 'src/app/components/sidebar/sidebar.module';
 import { SistemaComponent } from './sistema.component';
 import { SistemaRoutingModule } from './sistema-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   providers: [],
   declarations: [SistemaComponent],
-  imports: [CommonModule, SistemaRoutingModule, NavbarModule, SidebarModule],
+  imports: [
+    CommonModule,
+    SistemaRoutingModule,
+    NavbarModule,
+    SidebarModule,
+    ReactiveFormsModule,
+  ],
 })
 export class SistemaModule {}
